742c94a13caee989572e1672ce56a4ac9b025cb9
Kelsey <sweeneyk@bu.edu>
2026-03-24 22:07:15 -0400

    file structure

---
+++ csv_loader.py
@@ -0,0 +1,6 @@
+# Objective: Understand the structure of CSV and how it maps to SQL tables
+
+# Manually create a table in SQLite.
+# Use pandas.read_csv() to load data.
+# Insert data into  SQLite
+# Run basic queries using sqlite3 or DB browser.
---
+++ schema_manager.py
@@ -0,0 +1,3 @@
+# The database stores data. The Schema Manager understands the structure of that data.
+
+# Objective: Automate table creation by inferring schema from CSV.
