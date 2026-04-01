b4a5c876b05167fced2e77deff3adff5fff5907d
Kelsey <sweeneyk@bu.edu>
2026-04-01 14:23:51 -0400

    before I change things around

--- sql_db_validator/sql_db_validator_logic.py
+++ sql_db_validator/sql_db_validator_logic.py
@@ -2,6 +2,10 @@ def validate_LLM_output(table):
 
     return 0
 
+# verification and validation
 
 # no destructive querys (drop, inject, corrupt)
-#
+# query (connect only with read only for query) But can write via the sqlite_db
+
+
+# ensure the result is valie
